70139bd27bf8ce18ade79b6ab0cfdc0f372ba317
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2025-10-08 20:28:13 +0530

    created the first rest api i.e. hello api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here
-
+ 
 class HelloApiView(APIView):
     """Test api views"""
     def get(self,request, format=None):
